2acac57469aad84227b1732b0f19c3bc6c757740
chengxianghe <chengxianghe@lianluo.com>
2016-08-02 15:56:32 +0800

    init
    准备开发一个表情输入框

---
+++ XHEmoticonInputView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHEmoticonInputView
+//
+//  Created by chengxianghe on 16/8/2.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHEmoticonInputView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHEmoticonInputView
+//
+//  Created by chengxianghe on 16/8/2.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
